f541084f1a69aff7209013d132c76ada5e20794f
Prohor <prohor-yakovlev@mail.ru>
2022-11-25 20:03:56 +0300

    Test appWork

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,16 @@ function App(props: any) {
     <div>
       <PageTitle title={'This is APP component'} />
       <PageTitle title={'My friends'} />
-      {/*Article 1*/}
-      {/*<Rating value={3} />*/}
+      Article 1
+      <Rating value={3} />
       <Accordion titleValue={'Menu'} />
       <Accordion titleValue={'Users'} />
-      {/*<Rating value={0} />*/}
-      {/*<Rating value={1} />*/}
-      {/*<Rating value={2} />*/}
-      {/*<Rating value={3} />*/}
-      {/*<Rating value={4} />*/}
-      {/*<Rating value={5} />*/}
+      <Rating value={0} />
+      <Rating value={1} />
+      <Rating value={2} />
+      <Rating value={3} />
+      <Rating value={4} />
+      <Rating value={5} />
     </div>
   )
 }
