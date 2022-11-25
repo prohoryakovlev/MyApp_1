22ef60ad1b50ffab049ff9714f9c9292dbabe805
Prohor <prohor-yakovlev@mail.ru>
2022-11-25 20:01:34 +0300

    Test

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+type AccordionPropsType = {
+    titleValue: string
+}
+
+function Accordion(props: AccordionPropsType) {
+    console.log("Accordion render")
+    return <div>
+        <AccordionTitle title={props.titleValue}/>
+        <AccordionBody/>
+    </div>
+}
+
+type AccordionTitlePropsType = {
+    title: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    console.log("AccordionTitle YEAP")
+    return (
+        <h3>--- {props.title} ---</h3>
+    )
+}
+
+function AccordionBody() {
+    console.log("AccordionBody YEAP")
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
+export default Accordion;
